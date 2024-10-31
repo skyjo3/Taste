2368fe26a33aa140586c178c2117ce8f2a97344a
skyjo3 <skyjo3@gmail.com>
2024-10-31 15:25:18 -0700

    created 5 test case files

--- Taste/TasteTests/ListViewControllerTests.swift
+++ Taste/TasteTests/ListViewControllerTests.swift
@@ -1,14 +1,13 @@
 //
-//  TasteTests.swift
+//  ListViewControllerTests.swift
 //  TasteTests
 //
 //  Created by amy on 2024-10-31.
 //
 
 import XCTest
-@testable import Taste
 
-final class TasteTests: XCTestCase {
+final class ListViewControllerTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.

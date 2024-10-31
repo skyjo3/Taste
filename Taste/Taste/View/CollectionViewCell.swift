807a8bc3756552cd6b500d221a8e31e113b45818
skyjo3 <skyjo3@gmail.com>
2024-10-31 15:41:33 -0700

    added the skeleton codes for the CollectionViewCell, EmptyListView, and ErrorAlertController

--- Taste/Taste/View/CollectionViewCell.swift
+++ Taste/Taste/View/CollectionViewCell.swift
@@ -8,5 +8,17 @@
 import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
+    static let reuseIdentifier = "customCell"
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init coder has not been implemented!")
+    }
+    
+    func configure() {
+        
+    }
 }

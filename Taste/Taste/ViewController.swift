3eed3224f49a289beab8ceeb943b94a0ec691ad5
skyjo3 <skyjo3@gmail.com>
2024-10-31 15:11:02 -0700

    created the xcode project and MVC folders

---
+++ Taste/Taste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Taste
+//
+//  Created by amy on 2024-10-31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

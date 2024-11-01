64eabd9f70e428a7191f38d1b582c6e4c6ad6818
skyjo3 <skyjo3@gmail.com>
2024-10-31 17:02:33 -0700

    filled in the content of RecipeData based on the returned json format

--- Taste/Taste/Model/RecipeData.swift
+++ Taste/Taste/Model/RecipeData.swift
@@ -7,6 +7,16 @@
 
 import Foundation
 
-struct RecipeData {
+struct RecipeData: Codable {
+    let recipes: [Recipe]
     
+    struct Recipe: Codable {
+        let cuisine: String
+        let name: String
+        let photo_url_large: String
+        let photo_url_small: String
+        let source_url: String
+        let uuid: String
+        let youtube_url: String
+    }
 }

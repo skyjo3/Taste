3178f6e75b668c8606c45d05be17d20d200151dc
skyjo3 <skyjo3@gmail.com>
2024-11-01 16:33:54 -0700

    added conditions for JSONParsingTests

--- Taste/TasteTests/JSONParsingTests.swift
+++ Taste/TasteTests/JSONParsingTests.swift
@@ -37,16 +37,23 @@ final class JSONParsingTests: XCTestCase {
                 XCTAssertEqual(results[0].name, "Apam Balik")
                 XCTAssertEqual(results[0].cuisine, "Malaysian")
                 XCTAssertEqual(results[0].imageURL, "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b9ab0071-b281-4bee-b361-ec340d405320/large.jpg")
+                XCTAssertEqual(results[0].videoURL, "https://www.youtube.com/watch?v=6R8ffRRJcrg")
+                XCTAssertEqual(results[0].sourceURL, "https://www.nyonyacooking.com/recipes/apam-balik~SJ5WuvsDf9WQ")
                 
                 // last item
                 XCTAssertEqual(results[62].name, "White Chocolate Crème Brûlée")
                 XCTAssertEqual(results[62].cuisine, "French")
                 XCTAssertEqual(results[62].imageURL,  "https://d3jbb8n5wk0qxi.cloudfront.net/photos/f4b7b7d7-9671-410e-bf81-39a007ede535/large.jpg")
+                XCTAssertEqual(results[62].videoURL, "https://www.youtube.com/watch?v=LmJ0lsPLHDc")
+                XCTAssertEqual(results[62].sourceURL, "https://www.bbcgoodfood.com/recipes/2540/white-chocolate-crme-brle")
                 
                 // item somewhere in the middle
                 XCTAssertEqual(results[4].name, "Banana Pancakes")
                 XCTAssertEqual(results[4].cuisine, "American")
                 XCTAssertEqual(results[4].imageURL, "https://d3jbb8n5wk0qxi.cloudfront.net/photos/b6efe075-6982-4579-b8cf-013d2d1a461b/large.jpg")
+                XCTAssertEqual(results[4].videoURL, "https://www.youtube.com/watch?v=kSKtb2Sv-_U")
+                XCTAssertEqual(results[4].sourceURL, "https://www.bbcgoodfood.com/recipes/banana-pancakes")
+                
             } catch {
                 XCTFail("Failed to decode json file: \(error)")
             }

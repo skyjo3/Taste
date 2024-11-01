a2255f2669f1b2b7bc1ae9f1eb570d14c5ab5c40
skyjo3 <skyjo3@gmail.com>
2024-10-31 17:24:52 -0700

    added actual http request in fetchRecipes() in RecipeManager

--- Taste/Taste/Model/RecipeManager.swift
+++ Taste/Taste/Model/RecipeManager.swift
@@ -22,10 +22,31 @@ struct RecipeManager {
     }
     
     func fetchRecipes(with urlString: String) {
-        
+        if let url = URL(string: urlString) {
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
+                    return
+                }
+                if let safeData = data {
+                    let recipes = self.parseJSON(safeData)
+                    self.delegate?.didUpdateRecipes(self, recipes: recipes)
+                } else {
+                    let error = NSError(domain: "", code: 100, userInfo: [NSLocalizedDescriptionKey: "The request does not return any data."])
+                    delegate?.didFailWithError(error: error)
+                }
+            }
+            task.resume()
+        } else {
+            let error = NSError(domain: "", code: 100, userInfo: [NSLocalizedDescriptionKey: "The URL is invalid."])
+            delegate?.didFailWithError(error: error)
+        }
     }
     
     func parseJSON(_ recipeData: Data) -> [RecipeModel] {
+        
+        
+        
         return []
     }
 }

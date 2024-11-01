541041653a51c4bd24ab717f6371b433d12bc233
skyjo3 <skyjo3@gmail.com>
2024-11-01 11:08:12 -0700

    filled in the empty view

--- Taste/Taste/View/EmptyListView.swift
+++ Taste/Taste/View/EmptyListView.swift
@@ -12,8 +12,21 @@ class EmptyListView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        let imageView = UIImageView(frame: CGRectMake(0, 0, 50, 50))
+        imageView.center = self.center
+        imageView.image = UIImage(systemName: "mail.and.text.magnifyingglass")
+        imageView.tintColor = .black
+        imageView.contentMode = .scaleAspectFill
+        self.addSubview(imageView)
+        
+        let label = UITextView(frame: CGRectMake(0, 0, 200, 100))
+        label.center = CGPoint(x: imageView.center.x, y: imageView.center.y+90)
+        label.text = "No recipe at this moment.\n\nTry again later ;)"
+        label.font = UIFont.systemFont(ofSize: 12)
+        label.textAlignment = .center
+        self.addSubview(label)
     }
     required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented!")
+        fatalError("init(coder:) has not been implemented.")
     }
 }
